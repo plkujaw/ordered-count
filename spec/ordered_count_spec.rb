19e00e2ba1da8cf5637a67b8cbaffc7e6f988bb0
plkujaw <jawor.jakub@gmail.com>
2020-05-28 08:39:41 +0100

    add and pass more tests

--- spec/ordered_count_spec.rb
+++ spec/ordered_count_spec.rb
@@ -8,5 +8,7 @@ describe '#ordered_count' do
     expect(ordered_count('aabc')).to eq([['a', 2], ['b', 1], ['c', 1]])
     expect(ordered_count('aabcc')).to eq([['a', 2], ['b', 1], ['c', 2]])
     expect(ordered_count('aa cc')).to eq([['a', 2], [' ', 1], ['c', 2]])
+    expect(ordered_count('abracadabra')).to eq([['a', 5], ['b', 2], ['r', 2], ['c', 1], ['d', 1]])
+    expect(ordered_count("Code Wars")).to eq([['C', 1], ['o', 1], ['d', 1], ['e', 1], [' ', 1], ['W', 1], ['a', 1], ['r', 1], ['s', 1]])
   end
 end
