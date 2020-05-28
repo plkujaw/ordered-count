322ab1eb071a59527916e4e7cc50c1d41419c4d9
plkujaw <jawor.jakub@gmail.com>
2020-05-28 08:33:47 +0100

    add and pass more basic tests

--- spec/ordered_count_spec.rb
+++ spec/ordered_count_spec.rb
@@ -3,6 +3,8 @@ require 'ordered_count'
 describe '#ordered_count' do
   it  'returns the number of occurrences of each character and return it as a list of tuples in order of appearance' do
     expect(ordered_count('a')).to eq([['a', 1]])
-  
+    expect(ordered_count('ab')).to eq([['a', 1], ['b', 1]])
+    expect(ordered_count('abc')).to eq([['a', 1], ['b', 1], ['c', 1]])
+    
   end
 end
